8b15bbda46951166ddc070a4c39946b5769393df
mohamed <khaldimohamed82@gmail.com>
2025-04-12 13:10:13 +0100

    Expanding my Skillset, Embracing Continuous Learning

---
+++ fractol/fractol.c
@@ -0,0 +1,5 @@
+#include "fractol.h"
+
+int	main(int ac, char **av)
+{
+}
---
+++ fractol/fractol.h
@@ -0,0 +1,11 @@
+#ifndef FRACTOL_H
+# define FRACTOL_H
+
+typedef struct s_complex
+{
+	double	img;
+	double	r;
+
+}			t_complex;
+
+#endif
